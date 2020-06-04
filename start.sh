f4b68482fb5bf8964428346d7201a8643ebfa4e0
mario <mario@bevylabs.com>
2020-06-04 14:32:41 -0500

    Fix nginx start

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 NON_SECRET=$(gcloud secrets versions access 1 --secret="MARIO_MANUAL_SECRET")
 export MESSAGE=${NON_SECRET}
 echo "${MESSAGE}"
-envsubst < /nginx.conf.template > /etc/nginx/nginx.conf && exec nginx -g 'daemon off'
+envsubst < /nginx.conf.template > /etc/nginx/nginx.conf && exec nginx -g 'daemon off;'
